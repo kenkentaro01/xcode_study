2fee30bda0feb33b85e8afc82012ca9ceda052d6
kentaro <k.moco0109@gmail.com>
2024-01-15 01:34:13 +0900

    hello world

---
+++ Studyswift/Studyswift/StudyswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyswiftApp.swift
+//  Studyswift
+//
+//  Created by 健太郎 on 2024/01/15.
+//
+
+import SwiftUI
+
+@main
+struct StudyswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
