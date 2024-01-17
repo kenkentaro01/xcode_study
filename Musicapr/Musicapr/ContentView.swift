674b612a83570ddd81a232d841cca32c1edf460c
kentaro <k.moco0109@gmail.com>
2024-01-18 01:00:12 +0900

    音楽アプリ完成

--- Musicapr/Musicapr/ContentView.swift
+++ Musicapr/Musicapr/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+//    音を鳴らすためのsoundPlayerクラスのインスタンスを生成する
+    let soundPlayer = SoundPlayer()
     var body: some View {
         ZStack {
 //            背景画面の指定
@@ -21,11 +23,14 @@ struct ContentView: View {
 //                シンバルボタン
                 Button{
 //              ボタンをタップした時のアクション
+//                    シンバルの音を鳴らす
+                    soundPlayer.cymbalPlay()
                 }label: {
                     Image(.cymbal)
                 }
                 Button{
 //              ボタンをタップした時のアクション
+                    soundPlayer.guitarPlay()
                 }label: {
                     Image(.guitar)
                 }
--- Musicapr/Musicapr/SoundPlayer.swift
+++ Musicapr/Musicapr/SoundPlayer.swift
@@ -6,7 +6,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundPlayer: NSObject {
-
+//シンバル音源データを読み込み
+//    letは一度値を設定したら変更できないといった指定
+//    NSDataAssetはAsset .xcasset内のファイルや画像を管理してくれるクラス
+    let cymbalData = NSDataAsset(name: "cymbalSound")!.data
+    
+//    シンバル用プレイヤー変数
+//    [!]は強制アンラップ
+    var cymbalPlayer : AVAudioPlayer!
+    
+    func cymbalPlay(){
+//        tryとdoーcatchを組み合わせて例外処理を行います。
+        do{
+            cymbalPlayer = try AVAudioPlayer(data: cymbalData)
+            //        シンバルの音源を再生
+            cymbalPlayer.play()
+        }catch{
+            print("シンバルで、エラーが発生しました。")
+        }
+    }
+    
+    let guitarData = NSDataAsset(name: "guitarSound")!.data
+    
+    var guitarPlayer : AVAudioPlayer!
+    
+    func guitarPlay(){
+        do{
+            guitarPlayer = try AVAudioPlayer(data: guitarData)
+            
+            guitarPlayer.play()
+        }catch{
+            print("ギターでエラーが発生しました。")
+        }
+    }
 }
