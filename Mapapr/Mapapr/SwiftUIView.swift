dfdba8eb07b44682ef9eb55c74e6e3e0aac6b084
kentaro <k.moco0109@gmail.com>
2024-01-20 01:37:03 +0900

    Mapapr.Swiftの追加

---
+++ Mapapr/Mapapr/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//SwiftUIViewを選択することで
+//  SwiftUIView.swift
+//  Mapapr
+//
+//  Created by 健太郎 on 2024/01/20.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
