1d00e99a97388de9aaa7d76002a61455feb4a8a5
kentaro <k.moco0109@gmail.com>
2024-01-20 01:21:02 +0900

    mapアプリの作成

---
+++ Mapapr/Mapapr/MapaprApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapaprApp.swift
+//  Mapapr
+//
+//  Created by 健太郎 on 2024/01/20.
+//
+
+import SwiftUI
+
+@main
+struct MapaprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
