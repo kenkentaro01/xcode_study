86bfb8f4a2bc5ea1e57fa6b5e57f6ecbba5d708a
kentaro <k.moco0109@gmail.com>
2024-01-20 23:51:32 +0900

    マップ検索アプリの完成

--- Mapapr/Mapapr/ContentView.swift
+++ Mapapr/Mapapr/ContentView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+//    入力中の文字列を保持する状態変数
+    @State var inputText: String = ""
+    
+//    検索キーワードを保持する状態変数　初期値は東京駅
+    @State var displaySearchKey: String = "東京駅"
     var body: some View {
 //垂直にレイアウト
         VStack{
+//            テキストフィールド（文字入力）
+//            TextFieldの第一引数は説明
+            TextField("キーワード",text: $inputText,prompt: Text("キーワードを入力してください"))
+//            onSubmitは開業を行なったタイミングで{}内のコードが実行される
+//            入力が完了された時
+                .onSubmit {
+                    displaySearchKey = inputText
+                }
+                .padding()
 //            マップを表示
-            MapView(searchKey: "羽田空港")
+            MapView(searchKey: displaySearchKey)
         }
     }
 }
