f545f69704d9fe495ab9a8ab9d3c82c469eb8392
kentaro <k.moco0109@gmail.com>
2024-01-27 03:12:59 +0900

    カメラアプリの追加

---
+++ Cameraapr/Cameraapr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cameraapr
+//
+//  Created by 健太郎 on 2024/01/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
