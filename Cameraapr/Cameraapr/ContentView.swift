3af2a4e69f6ad732fcc063d20d3ceea4ffa7431e
kentaro <k.moco0109@gmail.com>
2024-01-28 17:21:26 +0900

    カメラの起動追加

--- Cameraapr/Cameraapr/ContentView.swift
+++ Cameraapr/Cameraapr/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+//    撮影した写真を穂s時する状態変数
+//    nilは写真がない状態を示している
+    @State var captureImage: UIImage? = nil
+//    撮影がめんの開閉状態を管理
+//    trueの時は画面を表示しする。
+    @State var isShowSheet = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//スペースの追加
+            Spacer()
+//            撮影した写真がある時
+            if let captureImage{
+//                撮影写真を表示
+//                Imageは画像を表示するためのViewを作成する
+                Image(uiImage: captureImage)
+                .resizable()
+                .scaledToFit()
+            }
+//            カメラを起動するボタン
+            Button{
+//                ボタンをタップしたときのアクション
+//                カメラが利用可能かチェック
+                if UIImagePickerController.isSourceTypeAvailable(.camera){
+                    print("カメラは使用できます")
+//                    カメラが使えるならisShowSheetをtrue
+//                    togleメソッドによりtrue falseを切り替えることができる
+                    isShowSheet.toggle()
+                }else {
+                    print("カメラ利用できません")
+                }
+            }label:{
+//                テキスト表示
+                Text("カメラを起動する")
+//                横幅いっぱい
+                    .frame(maxWidth: .infinity)
+//                 高さ50ポイントを指定
+                    .frame(height: 50)
+//                文字列をセンタリング指定
+                    .multilineTextAlignment(.center)
+//                背景を青色に指定
+                    .background(Color.blue)
+//                文字色を白色に指定
+                    .foregroundStyle(Color.white)
+            }
+            .padding()
+//            sheetを表示
+//            isPresentedで指定した状態変数がtrueの時実行
+            .sheet(isPresented: $isShowSheet){
+//                UIImagePickerControllerを表示
+                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+            }
         }
-        .padding()
+
     }
 }
 
--- Cameraapr/Cameraapr/ImagePikerView.swift
+++ Cameraapr/Cameraapr/ImagePikerView.swift
@@ -7,9 +7,10 @@
 
 import SwiftUI
 
-struct ImagePikerView: UIViewControllerRepresentable {
+struct ImagePickerView: UIViewControllerRepresentable {
 //UIViewPickerControllerが表示されているかを管理
 //    撮影画面の表示状態を管理する。撮影が終了したときやキャンセルボタンが押されたときに「false」を設定して画面を閉じる
+//    Bindingを使用することでView間でデータの同期を保つことができます。
     @Binding var isShowSheet : Bool
 //    撮影した写真を格納する変数
 //    画面に表示される画像や外部から取り込んだ画像を管理するために使用される
@@ -19,14 +20,21 @@ struct ImagePikerView: UIViewControllerRepresentable {
 //    Coodinatorでコントローラーのdelegateを管理
     class Coordinator : NSObject, UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 //        ImagePickerView型の定数を用意
-        let parent :ImagePikerView
+        let parent : ImagePickerView
 //        イニシャライザ
-        init(_ parent: ImagePikerView){
+//        22行目で宣言したparentのこと
+//        Coordinatorの親である「ImagePicker View」がself.paretに格納される
+//        イニシャライザは構造体やクラスのインスタンスの生成の際に初期化のために使用する手続きのこと
+        init(_ parent:ImagePickerView){
             self.parent = parent
         }
 //        撮影が終わったときに呼ばれるdelegateメソッド　必ず必要
+//        第一引数カメラ撮影を行う画面　第二引数　撮影した写真の情報を格納する
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 //            撮影した写真をcaptureImageに保存
+//            UIImagePickerController.InfoKey.originalImageを指定することでカメラで撮影した写真が取得できる。
+//            Any型についていろんなデータが格納できる。 「いろんな」→int stringでも入る
+//            as? UIImageにより「Any」→「UIImage」へ型を変換している　これをダウンキャストという
             if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
                 parent.captureImage = originalImage
             }
@@ -38,6 +46,33 @@ struct ImagePikerView: UIViewControllerRepresentable {
             parent.isShowSheet.toggle()
         }
     }
-    
+//    cordinatorを生成swiftUIによって自動的に呼び出し
+    func makeCoordinator() -> Coordinator {
+//cordinatorクラスのインスタンスの生成
+        Coordinator(self)
+    }
+//    Viewを生成するときに実行
+    func makeUIViewController(context: Context) -> UIViewController {
+//     UIViewControllerのインスタンスを生成
+        let myImagePickerContoroller = UIImagePickerController()
+//        sourceTypeにcameraを設定
+//        写真の取得先を指定。.cameraを指定することでカメラから写真を取得することができる
+        myImagePickerContoroller.sourceType = .camera
+//        delegate設定
+//        delegateを利用するために通知先を指定する必要がある。
+//        通知先　→「どこにdelegateメソッドがあるのか」をせ知らせるために指定する必要がある
+        myImagePickerContoroller.delegate = context.coordinator
+//        UIViewControllerを返す
+//        カメラの起動に必要なUIImagePickerControllerを返してカメラ撮影ができるように指示する。
+        return myImagePickerContoroller
+    }
+//    Viewが更新されたときに実行
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+//        処理なし
+    }
 }
 //didFinishPickingMediaWithInfo
+
+
+//UIImagePickerControllerについて
+//UIImagePickerControllerとはiOSデバイスに搭載されているカメラを使って写真や動画を撮影できる機能が提供されているクラス
