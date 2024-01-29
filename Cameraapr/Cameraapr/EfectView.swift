470a0f180114d7b9e16c839a325e6cc841e78249
kentaro <k.moco0109@gmail.com>
2024-01-30 01:03:54 +0900

    エフェクト追加

--- Cameraapr/Cameraapr/EfectView.swift
+++ Cameraapr/Cameraapr/EfectView.swift
@@ -17,6 +17,58 @@ struct EfectView: View {
 //    編集後の写真を保存しておく
             @State var showImage : UIImage?
     var body: some View {
+//        縦方向にレイアウト
+        VStack{
+            Spacer()
+            if let showImage{
+//                表示する写真があるばいいは画面に表示
+                Image(uiImage: showImage)
+                    .resizable()
+                    .scaledToFit()
+            }
+            Spacer()
+//            エフェクトボタン
+            Button{
+//                ボタンをタップした時のアクション
+            }label: {
+                Text("エフェクト")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundStyle(Color.white)
+            }
+            .padding()
+            
+            if let showImage{
+                let shareImage = Image(uiImage: showImage)
+//                共有シート
+                ShareLink(item:shareImage,subject: nil,message: nil,preview: SharePreview("Photo",image: shareImage)){
+                    Text("シェア")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .multilineTextAlignment(.center)
+                        .background(Color.blue)
+                        .foregroundStyle(Color.white)
+                }
+                .padding()
+            }
+            Button{
+//                ボタンをタップした時のアクション
+            }label: {
+                Text("閉じる")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundStyle(Color.white)
+            }
+            .padding()
+        }
+        .onAppear{
+//            撮影した写真を表示する写真に設定
+            showImage = captureImage
+        }
 
     }
 }
