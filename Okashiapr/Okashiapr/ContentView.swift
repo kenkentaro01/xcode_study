4fc2a7366fbd871e2b9aaabe9874c24d42384b8b
kentaro <k.moco0109@gmail.com>
2024-02-12 02:38:13 +0900

    同期　非同期処理

--- Okashiapr/Okashiapr/ContentView.swift
+++ Okashiapr/Okashiapr/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+//    OkashiDataを参照する変数
+    var OkashiDataList = OkashiData()
+//    入力された文字列を保持する状態変数
+    @State var  inputText = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+//            文字を受け取るTextfieldを表示する
+            TextField("キーワード",
+//                      ＄をつけることで状態変数の値を参照せしてる
+            text: $inputText,
+//                      プレースフォルダーに表示するメッセージ
+            prompt: Text("キーワードを入力してください"))
+//            textfieldの入力が確定したタイミングで実行される
+            .onSubmit {
+//                入力完了直後に検索をする
+                OkashiDataList.searchOkashi(keyword: inputText)
+            }
+//            キーボードの改行を検索に変更する
+            .submitLabel(.search)
+            .padding()
         }
-        .padding()
     }
 }
 
